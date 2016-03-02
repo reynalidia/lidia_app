30e639ee94ceda09db837d562e21d210fd17ab81
reynalidia chambilla chura <reynachambilla@gmail.com>
2016-03-02 09:13:43 -0500

    iniciando proceso

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+
+
+<h1>HOLA BIENVENIDOS A HERUKO</h1>
+
+</body>
+</html>
